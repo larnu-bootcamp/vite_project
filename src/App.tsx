3871d71e5a514cfdc3caf59e76d7757b52360d00
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-28 12:09:44 -0300

    rompiendo test

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>PROYECTO DE LA GENERACION 22 DE AGOSTO</h1>
+      <h1>PROYECTO DE LA GENERACION 21 DE AGOSTO</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
